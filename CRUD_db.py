8edea003f268ea7cc061f33b8d2925ecab9685e8
Luis Carlos Bezera <luiscarlossilvabezerra@gmail.com>
2024-04-11 18:46:41 -0300

    crud atualizado

--- CRUD_db.py
+++ CRUD_db.py
@@ -1,53 +1,87 @@
-import psycopg2
 from user_DB import DB_NAME, USER, PASSWORD, HOST, PORT
+import psycopg2
+from io import BytesIO
+from PIL import Image
+import matplotlib.pyplot as plt
 
-#-------------------------------------------------------------------------------------------------------
-#CONECTANDO AO BANCO DE DADOS
 try:
     conexao = psycopg2.connect(
-        dbname=DB_NAME,
-        user=USER,
-        password=PASSWORD,
-        host=HOST,
-        port=PORT
-        )
-    print("Conexão estabelecida com sucesso!")
-except Exception as e:
-    print(f"Ocorreu um erro: {e}")
+        dbname = DB_NAME,
+        user = USER,
+        password = PASSWORD,
+        host = HOST,
+        port = PORT
+    )
+    print('Conexão estabelecida com sucesso!')
+except Exception as error:
+    print(f'Falha ao conectar. Erro: {error}')
 
-#--------------------------------------------------------------------------------------------------------
-#Inserir dados na tabela DOCUMENTO_DIGITAL
-def inserir_dados_documento(nome_agente, localizacao_fisica, data_contrato, valor_credito, numero_cedula, id_imagem, id_cliente):
+
+#--------------------------------TABELA IMAGEM------------------------------------------
+#inserir imagem
+def inserir_imagem(image_path, image_name):
     """
-    Função para inserir os dados na tabela documento digital.
+    Inserts an image into the database.
 
     Args:
-    nome_agente (str): nome do agente registrado na cédula
-    localizacao_fisica (str): código do local onde se encontra a cédula física. ex RA05
-    data_contrato (str): data do contrato já em formato padrão de entrada do BD
-    valor_credito (float): valor da cédula de crédito
-    numero_cedula (str): numero identificador da cédula de crédito
-    imagem (str): caminho da imagem
-    cliente_id (int): id do cliente, apontando para a tabela cliente
-
+        image_path (str): Path of the image file.
+        image_name (str): Name of the image.
 
     Returns:
-    Sem retorno. A função apenas faz a inserção dos dados no banco de dados.
+        bool: True if insertion is successful, False otherwise.
     """
     try:
-        cursor = conexao.cursor()
-        # Executar a query com os valores fornecidos
-        cursor.execute(f'INSERT INTO Documento_Digital (nome_agente, localizacao_fisica, data_contrato, valor_credito, numero_cedula, id_imagem, id_cliente) VALUES (%s, %s, %s, %s, %s, %s, %s)', (nome_agente, localizacao_fisica, data_contrato, valor_credito, numero_cedula, id_imagem, id_cliente))
-        conexao.commit()
-        cursor.close()
-        
-        return True
-    except psycopg2.Error as e:
-        print("Erro ao inserir dados:", e)
+        with conexao.cursor() as cursor:
+            with open(image_path, "rb") as image_file:
+                img = image_file.read()
+
+                cursor.execute(
+                    f"""
+                    INSERT INTO imagem (nome_imagem, imagem) 
+                        VALUES ('{image_name}', {psycopg2.Binary(img)});
+                    """
+                )
+
+            print("INFO: Inserção realizada com sucesso.")
+            conexao.commit()
+            return True
+    except Exception as e:
+        print("INFO: Erro ao inserir imagem no banco de dados.")
+        print(f"ERROR: {e}")
         conexao.rollback()
         return False
-    
-#----------------------------------------------------------------------------------------------------------------------------------------- 
+#ler imagem
+def ler_imagem(image_id):
+    """
+    Reads an image from the database and displays it.
+
+    Args:
+        image_id (int): ID of the image to be read.
+
+    """
+    try:
+        with conexao.cursor() as cursor:
+            sql = "SELECT nome_imagem, imagem FROM imagens WHERE id_imagem = %s"
+            cursor.execute(sql, (image_id,))
+            registro = cursor.fetchone()
+
+            if registro is not None:
+                nome_imagem, dados_imagem = registro
+                imagem_stream = BytesIO(dados_imagem)
+                imagem = Image.open(imagem_stream)
+                nome_arquivo_saida = f"imagem_recuperada_{image_id}.jpg"
+                plt.imshow(imagem)
+                plt.show()
+                print(
+                    f"INFO: Imagem {nome_imagem} recuperada e salva como {nome_arquivo_saida}"
+                )
+            else:
+                print("INFO: Nenhuma imagem encontrada com o ID especificado.")
+    except Exception as e:
+        print("INFO: Erro ao ler imagem do banco de dados.")
+        print(f"ERROR: {e}")
+
+#--------------------------------TABELA CLIENTE------------------------------------------
 #Inserir dados na tabela CLIENTE
 def inserir_dados_cliente(nome, cpf, agencia, conta, endereco, data_nascimento):
     """
@@ -65,46 +99,50 @@ def inserir_dados_cliente(nome, cpf, agencia, conta, endereco, data_nascimento):
     Sem retorno. A função apenas faz a inserção dos dados na tabela cliente.
     """
     try:
-        cursor = conexao.cursor()
-        # Executar a query com os valores fornecidos
-        cursor.execute(f'INSERT INTO Cliente (nome, cpf, agencia, conta, endereco, data_nascimento) VALUES (%s, %s, %s, %s, %s, %s)', (nome, cpf, agencia, conta, endereco, data_nascimento))
-        conexao.commit()
-        cursor.close()
+        with conexao.cursor() as cursor:
+            cursor.execute(f'INSERT INTO Cliente (nome, cpf, agencia, conta, endereco, data_nascimento) VALUES (%s, %s, %s, %s, %s, %s)', (nome, cpf, agencia, conta, endereco, data_nascimento))
+            conexao.commit()
+            cursor.close()
+            print("Dados inseridos na tabela CLIENTE")
+            return True
         
-        return True
-    except psycopg2.Error as e:
+    except Exception as e:
         print("Erro ao inserir dados:", e)
         conexao.rollback()
         return False
 
-#----------------------------------------------------------------------------------------------------------------------------------------- 
-#Inserir dados na tabela IMAGEM
-def inserir_dados_imagem(nome_imagem, imagem, id_cliente):
+
+#--------------------------------TABELA DOCUMENTO------------------------------------------
+def inserir_dados_documento(nome_agente, localizacao_fisica, data_contrato, valor_credito, numero_cedula, id_imagem, id_cliente):
     """
-    Função para inserir dados na tabela imagem.
+    Função para inserir os dados na tabela documento digital.
 
     Args:
-    nome_imagem (str): nome da imagem
+    nome_agente (str): nome do agente registrado na cédula
+    localizacao_fisica (str): código do local onde se encontra a cédula física. ex RA05
+    data_contrato (str): data do contrato já em formato padrão de entrada do BD
+    valor_credito (float): valor da cédula de crédito
+    numero_cedula (str): numero identificador da cédula de crédito
     imagem (str): caminho da imagem
-    id_cliente (str): id do cliente da imagem
-    
+    cliente_id (int): id do cliente, apontando para a tabela cliente
+
     Returns:
-    Sem retorno. A função apenas faz a inserção dos dados na tabela imagem.
+    Sem retorno. A função apenas faz a inserção dos dados no banco de dados.
     """
     try:
-        cursor = conexao.cursor()
-        # Executar a query com os valores fornecidos
-        cursor.execute(f'INSERT INTO Imagem (nome_imagem, imagem, id_cliente) VALUES (%s, %s, %s)', (nome_imagem, imagem, id_cliente))
-        conexao.commit()
-        cursor.close()
+        with conexao.cursor() as cursor:
+            cursor.execute(f'INSERT INTO Documento_Digital (nome_agente, localizacao_fisica, data_contrato, valor_credito, numero_cedula, id_imagem, id_cliente) VALUES (%s, %s, %s, %s, %s, %s, %s)', (nome_agente, localizacao_fisica, data_contrato, valor_credito, numero_cedula, id_imagem, id_cliente))
+            conexao.commit()
+            cursor.close()
+            print("INFO: Inserção realizada com sucesso.")
+            return True
         
-        return True
-    except psycopg2.Error as e:
+    except Exception as e:
         print("Erro ao inserir dados:", e)
         conexao.rollback()
         return False
 
-#-----------------------------------------------------------------------------------------------------------------------------------------   
+#--------------------------------TABELA USUARIO------------------------------------------
 #Inserir dados na tabela USUARIO
 def inserir_dados_usuario(nome, cpf, user_name, senha):
     """
@@ -120,20 +158,20 @@ def inserir_dados_usuario(nome, cpf, user_name, senha):
     Sem retorno. A função apenas faz a inserção dos dados na tabela usuário.
     """
     try:
-        cursor = conexao.cursor()
-        # Executar a query com os valores fornecidos
-        cursor.execute(f'INSERT INTO Usuario (nome, cpf, user_name, senha) VALUES (%s, %s, %s, %s)', (nome, cpf, user_name, senha))
-        conexao.commit()
-        cursor.close()
+        with conexao.cursor() as cursor:
+            cursor.execute(f'INSERT INTO Usuario (nome, cpf, user_name, senha) VALUES (%s, %s, %s, %s)', (nome, cpf, user_name, senha))
+            conexao.commit()
+            cursor.close()
+            print('Dados inseridos na tabela USUARIO')
         
         return True
-    except psycopg2.Error as e:
+    except Exception as e:
         print("Erro ao inserir dados:", e)
         conexao.rollback()
         return False
 
-#----------------------------------------------------------------------------------------------------------------------------------------- 
-#Inserir dados na tabela APLICACAO_MANIPULA
+#--------------------------------TABELA APLICACAO------------------------------------------
+#inserir dados
 def inserir_dados_aplicacao(tipo_manipulacao, data_hora, id_usuario, id_documento):
     """
     Função para inserir dados na tabela usuário.
@@ -148,18 +186,18 @@ def inserir_dados_aplicacao(tipo_manipulacao, data_hora, id_usuario, id_document
     Sem retorno. A função apenas faz a inserção dos dados na aplicacao.
     """
     try:
-        cursor = conexao.cursor()
-        # Executar a query com os valores fornecidos
-        cursor.execute(f'INSERT INTO Aplicacao_Manipula (tipo_manipulacao, data_hora, id_usuario, id_documento) VALUES (%s, %s, %s, %s)', (tipo_manipulacao, data_hora, id_usuario, id_documento))
-        conexao.commit()
-        cursor.close()
+        with conexao.cursor() as cursor:
+            cursor.execute(f'INSERT INTO Aplicacao_Manipula (tipo_manipulacao, data_hora, id_usuario, id_documento) VALUES (%s, %s, %s, %s)', (tipo_manipulacao, data_hora, id_usuario, id_documento))
+            conexao.commit()
+            cursor.close()
+            print('Dados inseridos na tabela USUARIO')
         
         return True
-    except psycopg2.Error as e:
+    except Exception as e:
         print("Erro ao inserir dados:", e)
         conexao.rollback()
         return False
-
+    
 if __name__ == "__main__":
     #VARIÁVEIS PARA TESTE INSERIR DOCUMENTO
     nome_agente = 'Luciano Mendes' 
@@ -169,10 +207,46 @@ if __name__ == "__main__":
     numero_cedula = '002458-87'
     id_imagem = 3
     id_cliente = 5
+    imagem = 'ImagensTeste/IMG_20240403_163932.jpg'
 
     
-    # inserir_dados_imagem('cedula01','c:/users/luis/documentos/cedula01.jpg', 1 )
+    inserir_imagem(imagem, 'cedula01')
     # inserir_dados_cliente('Luis Carlos da Silva Bezerra', '002.754.533-40', '5555-5', '22222-0', 'Rua José Carneiro da Silva, n1234', '1985/11/26')
     # inserir_dados_documento(nome_agente, localizacao_fisica, data_contrato, valor_credito, numero_cedula, id_imagem, id_cliente)
     # inserir_dados_usuario('Fulano de Tal', '888.777.222-99', 'user01', 'abc123')
     # inserir_dados_aplicacao('Inserção', '2023/12/15 15:55', 1, 1)
+   
+
+'''
+#------------------------------------------------ANTIGO---------------------------------------
+
+
+#FUNÇÕES DE ATUALIZAÇÃO DO BANCO DE DADOS
+#GENÉRICA:
+#avaliar a escolha para atualição
+def atualizar_dados_por_id(nome_tabela, id, **valores):
+    # Construindo a parte SET da consulta SQL
+    set_values = ', '.join([f"{coluna} = %s" for coluna in valores.keys()])
+    
+    # Construindo a consulta SQL completa
+    consulta = f"UPDATE {nome_tabela} SET {set_values} WHERE id = %s"
+    
+    # Montando os valores a serem substituídos na consulta SQL
+    valores_para_substituir = list(valores.values()) + [id]
+    
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(consulta, valores_para_substituir)
+        conexao.commit()
+        print('Dados atualizados com sucesso!')
+        return True
+    
+    except Exception as error:
+        print(f'Falha ao atualizar os dados. Erro: {error}')
+        conexao.rollback()
+        return False
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+
+ 
+'''
