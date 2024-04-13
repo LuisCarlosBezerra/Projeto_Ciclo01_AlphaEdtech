18441397d9a88172f0e8d19db4ee0688f67b6149
rubenfreita <rubensfreitas65@gmail.com>
2024-04-12 21:29:47 -0300

    changed the user.py file

--- models/user.py
+++ models/user.py
@@ -1,4 +1,17 @@
 class User:
+    """
+    Represents a user entity.
+
+    This class encapsulates information about a user, including their name, CPF,
+    username, and password.
+
+    Attributes:
+        id (int): The unique identifier of the user.
+        name (str): The name of the user.
+        cpf (str): The CPF (Cadastro de Pessoas Físicas) of the user.
+        user_name (str): The username of the user.
+        password (str): The password of the user.
+    """
 
     def __init__(
         self,
@@ -8,6 +21,16 @@ class User:
         password: str,
         id: int = None,
     ) -> None:
+        """
+        Initializes a User instance with the provided attributes.
+
+        Args:
+            name (str): The name of the user.
+            cpf (str): The CPF (Cadastro de Pessoas Físicas) of the user.
+            user_name (str): The username of the user.
+            password (str): The password of the user.
+            id (int, optional): The unique identifier of the user.
+        """
         self.id = id
         self.name = name
         self.cpf = cpf
@@ -15,6 +38,12 @@ class User:
         self.password = password
 
     def save_to_database(self, db):
+        """
+        Saves the user instance to the database.
+
+        Args:
+            db (DatabaseConnection): The database connection object.
+        """
         insert_query = """
         INSERT INTO usuario (nome, cpf, user_name, senha) 
             VALUES (%s, %s, %s, %s) RETURNING id_usuario;
@@ -31,6 +60,16 @@ class User:
 
     @staticmethod
     def from_database(db, client_id):
+        """
+        Retrieves a User instance from the database based on the provided user ID.
+
+        Args:
+            db (DatabaseConnection): The database connection object.
+            client_id (int): The ID of the user to retrieve.
+
+        Returns:
+            User: The User instance retrieved from the database.
+        """
         select_query = """
         SELECT id_usuario, nome, cpf, user_name, senha FROM usuario
         WHERE usuario.id_usuario = %s;
@@ -56,4 +95,10 @@ class User:
             return None
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the User instance.
+
+        Returns:
+            str: A string representation of the User instance.
+        """
         return f"Usuario = ( id = {self.id}, nome = {self.name}, cpf = {self.cpf}, user_name = {self.user_name}, senha = {self.password})"
