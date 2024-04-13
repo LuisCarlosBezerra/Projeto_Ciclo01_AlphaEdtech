1a221e511f96647eeaebf7aec041b37c3743e8ff
rubenfreita <rubensfreitas65@gmail.com>
2024-04-12 21:28:12 -0300

    changed the client.py file

--- models/client.py
+++ models/client.py
@@ -2,6 +2,21 @@ from datetime import date
 
 
 class Client:
+    """
+    Represents a client entity.
+
+    This class encapsulates information about a client, including their name, CPF,
+    agency, account, address, and birth date.
+
+    Attributes:
+        id (int): The unique identifier of the client.
+        name (str): The name of the client.
+        cpf (str): The CPF (Cadastro de Pessoas Físicas) of the client.
+        agency (str): The agency associated with the client.
+        account (str): The account number associated with the client.
+        address (str): The address of the client.
+        birth_date (date): The birth date of the client.
+    """
 
     def __init__(
         self,
@@ -13,6 +28,18 @@ class Client:
         birth_date: date,
         id: int = None,
     ) -> None:
+        """
+        Initializes a Client instance with the provided attributes.
+
+        Args:
+            name (str): The name of the client.
+            cpf (str): The CPF (Cadastro de Pessoas Físicas) of the client.
+            agency (str): The agency associated with the client.
+            account (str): The account number associated with the client.
+            address (str): The address of the client.
+            birth_date (date): The birth date of the client.
+            id (int, optional): The unique identifier of the client.
+        """
         self.id = id
         self.name = name
         self.cpf = cpf
@@ -22,6 +49,13 @@ class Client:
         self.birth_date = birth_date
 
     def save_to_database(self, db):
+        """
+        Saves the client instance to the database.
+
+        Args:
+            db (DatabaseConnection): The database connection object.
+        """
+
         insert_query = """
         INSERT INTO cliente (nome, cpf, agencia, conta, endereco, data_nascimento) 
             VALUES (%s, %s, %s, %s, %s, %s) RETURNING id_cliente;
@@ -40,6 +74,16 @@ class Client:
 
     @staticmethod
     def from_database(db, client_id):
+        """
+        Retrieves a Client instance from the database based on the provided client ID.
+
+        Args:
+            db (DatabaseConnection): The database connection object.
+            client_id (int): The ID of the client to retrieve.
+
+        Returns:
+            Client: The Client instance retrieved from the database.
+        """
         select_query = """
         SELECT id_cliente, nome, cpf, agencia, conta, endereco, data_nascimento FROM cliente
         WHERE cliente.id_cliente = %s;
@@ -69,4 +113,10 @@ class Client:
             return None
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the Client instance.
+
+        Returns:
+            str: A string representation of the Client instance.
+        """
         return f"Cliente = ( id = {self.id}, nome = {self.name}, cpf = {self.cpf}, agencia = {self.agency}, conta = {self.account}, endereco = {self.address}, nasciimento = {self.birth_date})"
