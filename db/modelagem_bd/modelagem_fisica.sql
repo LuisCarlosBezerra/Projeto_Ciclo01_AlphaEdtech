0ad6e12438312b39d52d31653011cd97c637967e
Luis Carlos Bezera <luiscarlossilvabezerra@gmail.com>
2024-04-16 19:08:37 -0300

    update to BD

--- db/modelagem_bd/modelagem_fisica.sql
+++ db/modelagem_bd/modelagem_fisica.sql
@@ -1,32 +1,62 @@
-/* modelagem_logica: */
-CREATE TABLE Usuario ( id INTEGER PRIMARY KEY,
-                                          nome VARCHAR, cpf VARCHAR, user_name VARCHAR, senha VARCHAR);
+CREATE TABLE Cliente (
+		id_cliente SERIAL PRIMARY KEY UNIQUE,
+		nome VARCHAR NOT NULL,
+		cpf VARCHAR NOT NULL UNIQUE,
+		agencia VARCHAR NOT NULL,
+		conta VARCHAR NOT NULL,
+		endereco VARCHAR NOT NULL,
+		data_nascimento DATE NOT NULL
+);
 
+CREATE TABLE Documento_Digital (
+		id_documento SERIAL PRIMARY KEY UNIQUE,
+		nome_agente VARCHAR NOT NULL,
+		localizacao_fisica VARCHAR NOT NULL,
+		data_contrato DATE NOT NULL,
+		valor_credito DECIMAL NOT NULL,
+		numero_cedula VARCHAR NOT NULL,
+		id_imagem INTEGER NOT NULL,
+		id_cliente INTEGER NOT NULL
+);
 
-CREATE TABLE Documento_Digital ( id INTEGER PRIMARY KEY,
-                                                    nome_agente VARCHAR, localizacao_fisica VARCHAR, data_contrato DATE, valor_credito DECIMAL, numero_cedula INTEGER, arquivo_imagem BYTEA,
-                                                                                                                                                                       fk_Cliente_id INTEGER);
+CREATE TABLE Usuario (
+		id_usuario SERIAL PRIMARY KEY UNIQUE,
+		nome VARCHAR NOT NULL,
+		cpf VARCHAR NOT NULL UNIQUE,
+		user_name VARCHAR NOT NULL UNIQUE,
+		senha VARCHAR NOT NULL
+);
 
+CREATE TABLE Aplicacao_Manipula (
+		id_man SERIAL PRIMARY KEY UNIQUE,
+		tipo_manipulacao VARCHAR NOT NULL,
+		data_hora TIMESTAMP NOT NULL,
+		id_usuario INTEGER NOT NULL,
+		id_documento INTEGER NOT NULL
+);
 
-CREATE TABLE Cliente ( id INTEGER PRIMARY KEY,
-                                          nome VARCHAR, cpf VARCHAR, agencia VARCHAR, conta VARCHAR, endereco VARCHAR, data_nascimento DATE, UNIQUE (cpf,
-                                                                                                                                                     conta));
+CREATE TABLE Imagem (
+		id_imagem SERIAL PRIMARY KEY UNIQUE,
+		nome_imagem VARCHAR NOT NULL,
+		imagem BYTEA NOT NULL,
+);
 
+ALTER TABLE Aplicacao_Manipula
+ADD CONSTRAINT fk_id_documento
+FOREIGN KEY (id_documento)
+REFERENCES Documento_Digital(id_documento);
 
-CREATE TABLE Aplicacao_Manipula ( id INTEGER PRIMARY KEY,
-                                                     tipo_manipulacao VARCHAR, data_hora TIMESTAMP,
-                                                                                         fk_Usuario_id INTEGER, fk_Documento_Digital_id INTEGER);
+ALTER TABLE Aplicacao_Manipula
+ADD CONSTRAINT fk_id_usuario
+FOREIGN KEY (id_usuario)
+REFERENCES Usuario(id_usuario);
 
+ALTER TABLE Documento_Digital
+ADD CONSTRAINT fk_id_cliente
+FOREIGN KEY (id_cliente)
+REFERENCES Cliente(id_cliente);
 
-ALTER TABLE Documento_Digital ADD CONSTRAINT FK_Documento_Digital_2
-FOREIGN KEY (fk_Cliente_id) REFERENCES Cliente (id) ON
-DELETE RESTRICT;
-
-
-ALTER TABLE Aplicacao_Manipula ADD CONSTRAINT FK_Aplicacao_Manipula_2
-FOREIGN KEY (fk_Usuario_id) REFERENCES Usuario (id);
-
-
-ALTER TABLE Aplicacao_Manipula ADD CONSTRAINT FK_Aplicacao_Manipula_3
-FOREIGN KEY (fk_Documento_Digital_id) REFERENCES Documento_Digital (id);
-
+ALTER TABLE Documento_Digital
+ADD CONSTRAINT fk_id_imagem
+FOREIGN KEY (id_imagem)
+REFERENCES Imagem(id_imagem);
